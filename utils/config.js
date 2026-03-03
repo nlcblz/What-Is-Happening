4bc64784d5747f8d131ff5a097371f3614d2089c
Happy King <nlcblz@qq.com>
2026-03-03 13:08:38 +0800

    chore: 配置云托管环境 ID wih-server-4g7efso9461f6cf4 前端 callContainer 指向新账号云环境

--- utils/config.js
+++ utils/config.js
@@ -15,8 +15,8 @@ module.exports = {
   // 运行模式：'cloud' = 云托管 (wx.cloud.callContainer), 'local' = 本地开发 (wx.request)
   mode: 'cloud',
 
-  // 云托管环境 ID — 部署后在云托管控制台获取，填入此处
-  cloudEnv: '',
+  // 云托管环境 ID
+  cloudEnv: 'wih-server-4g7efso9461f6cf4',
 
   // 云托管服务名称
   cloudService: 'wih-server'
